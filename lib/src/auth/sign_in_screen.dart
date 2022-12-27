b29657b57be9942b92e0eb312da12784643cf6c6
Juliano Tavares <juliano.tavares@afinz.com.br>
2022-12-27 20:27:00 -0300

    S2A2: Sign In screen

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(children: [
+        Expanded(
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(45),
+              ),
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
